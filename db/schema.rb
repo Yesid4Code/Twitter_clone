11badecbec994d8ce573821b807c1c0b2015d5e9
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-03-09 17:11:20 -0500

    attribute 'text' added to tweets tablet

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_213141) do
+ActiveRecord::Schema.define(version: 2021_03_09_220910) do
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "teet"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
