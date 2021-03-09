e4a523f62a91af566b50e3132c508ae266ee9cf0
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-03-09 18:42:38 -0500

    redirection for devise controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :tweets
-  devise_for :users
+  devise_for :users, controllers: {
+               sesseions: 'users/sessions'
+             }
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
