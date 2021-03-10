e38485bbd1ada4d3bbd3410ee0798daea98b3069
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-03-09 19:13:29 -0500

    name of the field changed on the sign_up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets, dependent: :destroy
+  before_save :sinitize_text
+  
+  validates :username, presence: true,
+            length: {minimum: 5, maximum: 25}
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            length: {maximum: 50},
+            format: {with: VALID_EMAIL_REGEX}
+  
+
+
+  def sinitize_text
+    self.username = username.downcase
+    self.name = name.titleize
+    self.email = email.downcase
+  end
+  #end
 end
