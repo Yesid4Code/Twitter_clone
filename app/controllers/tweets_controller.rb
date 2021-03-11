2501457b71198d2f84cbe65885d1a9534191b0b9
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-03-11 02:40:54 -0500

    number of items added

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @tweets = Tweet.all.order("created_at DESC").paginate(page: params[:page], per_page: 3)
+    @tweets = Tweet.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
     @tweet = Tweet.new
   end
 
