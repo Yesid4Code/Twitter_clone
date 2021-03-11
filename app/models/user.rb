39ebb638253e9cb51049195907018830f6adb52e
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-03-11 15:35:50 -0500

    Methods to follow and unfollow users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :tweets, dependent: :destroy
-  before_save :sinitize_text
-  
+         :recoverable, :rememberable, :validatable  
   validates :username, presence: true,
             length: {minimum: 5, maximum: 25}
   validates :name, presence: true
@@ -14,12 +11,31 @@ class User < ApplicationRecord
             length: {maximum: 50},
             format: {with: VALID_EMAIL_REGEX}
   
+  before_save :sinitize_text
+  has_many :tweets, dependent: :destroy
+  has_many :active_relationship, class_name: "Relationship", foreign_key: "follower_id", dependet: :destroy
+  has_many :following, through: :active_relationship, source: followed
 
+  # follow another user
+  def follow(other_id)
+    active_relationship.create(followed_id: other_id)
+  end
 
+  #  unfollow a user
+  def unfollow(other_id)
+    active_relationship.find_by(followed_id: other_id).destroy
+  end
+
+  # is following a user?
+  def following?(other_id)
+    following.include?(other_id)
+  end
+  
   def sinitize_text
     self.username = username.downcase
     self.name = name.titleize
     self.email = email.downcase
   end
+
   #end
 end
