df466692f9f56157bc01730e90c486074c30a2b6
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-03-11 15:06:36 -0500

    improve the views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :tweets
-  devise_for :users, controllers: {
-               sesseions: 'users/sessions'
-             }
+  devise_for :users
+  #, controllers: {
+  #             sesseions: 'users/sessions'
+  #           }
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
