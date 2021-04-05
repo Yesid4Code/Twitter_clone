8eff0378d2c086e7b064db4111419d560625a467
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-04-05 11:30:41 -0400

    Instances of tweets and users added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   include UserScoped
 
   def show
+    @tweets = @user.tweets.paginate(page: params[:page], per_page: 10)
+    @users = User.all
   end
 
 end
